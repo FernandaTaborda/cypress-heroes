eba9e6968f43e1b930df63934411ee6be2c14e6b
Fernanda Taborda <carneirotaborda@hotmail.com>
2024-09-13 16:07:37 +0100

    added hire automation

--- client/cypress/e2e/testsFernandaTaborda.spec.cy.js
+++ client/cypress/e2e/testsFernandaTaborda.spec.cy.js
@@ -1,7 +1,9 @@
 import loginPage from '../Pages/loginPage.js'
 import userData from '../fixtures/userData.json'
+import hirePage from '../Pages/hirePage.js'
 
 const login = new loginPage
+const hire = new hirePage
 
 describe('Login', () => {
   it('Successful login', () => {
@@ -11,5 +13,20 @@ describe('Login', () => {
   it('Failed login', () => {
     login.loginWithAnyUser(userData.failedLogin)
     login.failedLogin()
+  })
+})
+
+describe('Hiring a Hero', () => {
+  it('Hire without logging in', () => {
+    hire.page()
+    hire.clickHireUser()
+    hire.errorClick()
+    hire.clickLikeUser()
+    hire.errorClick()
+  })
+  it('Hire after logging in', () => {
+    login.loginWithAnyUser(userData.successfulLogin)
+    hire.clickHireUser()
+    hire.succssfulHire()
   })
 })
