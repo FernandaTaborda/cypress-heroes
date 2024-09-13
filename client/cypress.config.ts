ab0623989dd8996b82a268bf813cb7de7a767baf
Fernanda Taborda <carneirotaborda@hotmail.com>
2024-09-13 15:47:14 +0100

    new password

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 import { createHero, deleteHero } from './cypress/support/data';
 
 export default defineConfig({
-  projectId: '0fd97c03-3a35-42cd-9c53-7c2e3dfc4bae',
+  projectId: 'm6usvo',
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
